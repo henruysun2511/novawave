30bc4fad8432e8087182b0d732666d3c767dd17a
huysun <huysun2511@gmail.com>
2025-11-19 13:48:38 +0700

    fix video 2

--- components/client/SongInfo/song-info.tsx
+++ components/client/SongInfo/song-info.tsx
@@ -1,14 +1,14 @@
 export default function SongInfo() {
     return (
         <div className="bg-[var(--background-secondary)] rounded-2xl overflow-hidden relative w-full h-[850px]">
-            {/* <video
+            <video
                 className="absolute top-0 left-0 w-full h-full object-cover"
                 autoPlay
                 loop
                 muted
             >
                 <source src="/videos/Download.mp4" type="video/mp4" />
-            </video> */}
+            </video>
             <div>Hello</div>
         </div>
     )
