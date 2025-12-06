052d861b84c5b975fa313876542dae19e29e0cd5
huysun <huysun2511@gmail.com>
2025-12-06 23:45:07 +0700

    fix auth provider

--- components/route/AuthInit.tsx
+++ components/route/AuthInit.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { authService } from "@/services/auth.service";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { useEffect, useState } from "react";
 
@@ -25,14 +24,14 @@ export default function AuthInitializer({
     setAccessToken(token);
 
     // Fetch thông tin người dùng
-    authService
-      .me()
-      .then((user) => {
-        setUser(user);
-      })
-      .finally(() => {
-        setReady(true);
-      });
+    // authService
+    //   .me()
+    //   .then((user) => {
+    //     setUser(user);
+    //   })
+    //   .finally(() => {
+    //     setReady(true);
+    //   });
   }, []);
 
   if (!ready) return null; 
