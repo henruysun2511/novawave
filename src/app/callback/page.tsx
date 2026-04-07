0b950adca4124f1de67feddaaf6e7e2c4aed4d81
huysun <huysun2511@gmail.com>
2026-04-07 15:54:07 +0700

    fix(auth): fix google callback

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,34 +1,34 @@
-"use client";
+// "use client";
 
-import { getCookieValue } from "@/libs/getCookieValue";
-import { useAuthStore } from "@/stores/useAuthStore";
-import { UserJwtPayload } from "@/types/body.type";
-import { jwtDecode } from "jwt-decode";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+// import { getCookieValue } from "@/libs/getCookieValue";
+// import { useAuthStore } from "@/stores/useAuthStore";
+// import { UserJwtPayload } from "@/types/body.type";
+// import { jwtDecode } from "jwt-decode";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { useEffect } from "react";
 
-export default function GoogleCallbackPage() {
-  const params = useSearchParams();
-  const router = useRouter();
-  const setAuth = useAuthStore((s) => s.setAuth);
+// export default function GoogleCallbackPage() {
+//   const params = useSearchParams();
+//   const router = useRouter();
+//   const setAuth = useAuthStore((s) => s.setAuth);
 
-  useEffect(() => {
+//   useEffect(() => {
 
-    const token = getCookieValue("accessToken");
+//     const token = getCookieValue("accessToken");
 
-    if (!token) {
-      router.push("/auth/login");
-      return;
-    }
+//     if (!token) {
+//       router.push("/auth/login");
+//       return;
+//     }
 
-    try {
-      const user = jwtDecode<UserJwtPayload>(token);
-      setAuth(token, user);
-      router.replace("/");
-    } catch {
-      router.push("/auth/login");
-    }
-  }, []);
+//     try {
+//       const user = jwtDecode<UserJwtPayload>(token);
+//       setAuth(token, user);
+//       router.replace("/");
+//     } catch {
+//       router.push("/auth/login");
+//     }
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
